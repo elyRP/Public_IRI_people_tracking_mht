77034b02688c8f192d638647430192d4ef52bc35
Ely Repiso <ely.repiso@upc.edu>
2025-10-31 19:00:51 +0100

    Copy right every were in code, not only in general package

--- tracking_nodes_dependencies_in_readme/ROSnodes/perception/iri_people_tracking_mht/include/people_tracking_mht_alg_node.h
+++ tracking_nodes_dependencies_in_readme/ROSnodes/perception/iri_people_tracking_mht/include/people_tracking_mht_alg_node.h
@@ -1,27 +1,50 @@
-// Copyright (C) 2010-2011 Institut de Robotica i Informatica Industrial, CSIC-UPC.
+// Copyright (C) from 2013-until now Institut de Robotica i Informatica Industrial, CSIC-UPC.
 // Author 
 // All rights reserved.
-//
-// This file is part of iri-ros-pkg
-// iri-ros-pkg is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
-// IMPORTANT NOTE: This code has been generated through a script from the 
-// iri_ros_scripts. Please do NOT delete any comments to guarantee the correctness
-// of the scripts. ROS topics can be easly add by using those scripts. Please
-// refer to the IRI wiki page for more information:
-// http://wikiri.upc.es/index.php/Robotics_Lab
-
+/*
+* people_tracking_mht_alg_node.h
+*
+*      Created on: 2013 by Ely Repiso and published first as her TFC on 13/12/2013. Last Modified by Ely Repiso on 2025 (migration to ros-Noetic and in the middle of ros2- humble migration) 
+*      Author: Ely Repiso (from 2013 and currently).
+*      Furthermore, this code is a modification and extension extracted from the theory of the open source papers of Donald Reid IEEE Transaction on Automatic Control 1979 and Kai Oliver Arras ICRA2008. We never had their code, therefore, we implemented this code from scratch, only taking inspiration from their articles.
+*      License (for other authors that will not be the original one): CC BY-NC-ND 4.0 
+*      (Attribution-NonCommercial-#NoDerivatives 4.0 International)
+*       https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en
+*      This license does not allow other authors to modify or to take profit from these works. 
+*      Then, for #modifications or derivative works, please contact ely.repiso@upc.edu to try to agree on 
+*  collaborations (for #journals with other researchers, formal collaborations between UPC and companies, and so on).
+*
+* Please, to use it cite: Vaquero, Victor, et al. "Low cost, robust and real time system for detecting and tracking moving objects to automate cargo handling in port terminals." Robot 2015: Second Iberian Robotics Conference: Advances in Robotics, Volume 2. Cham: Springer International Publishing, 2015.
+*  Redistribution and use in source and binary forms, without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*   * Additionally, remember that the derivatives without collaboration 
+*     Of the original authors are prohibited
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+***********************************************************
+*/
 #ifndef _people_tracking_mht_alg_node_h_
 #define _people_tracking_mht_alg_node_h_
 
--- tracking_nodes_dependencies_in_readme/c++libraries/tracking_library_trunk/include/prediction_bhmip.h
+++ tracking_nodes_dependencies_in_readme/c++libraries/tracking_library_trunk/include/prediction_bhmip.h
@@ -1,3 +1,46 @@
+/*
+ * prediction_bhmip.h
+ *
+ *  Created on: Jul 31, 2013. Last Modified by Ely Repiso on 2025 (migration to ros-Noetic and in the middle of ros2-humble migration).
+ *      Author: gferrer
+ *      Author: Ely Repiso (Modifications for people tracking mht)
+ *
+ *      license (for other authors that will not be the original ones): CC BY-NC-ND 4.0 
+ *              (Attribution-NonCommercial-NoDerivatives 4.0 International)
+ *              https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en
+ *      This license does not allow other authors to modify or to take profit from these works. 
+ *      Then, for modifications or derivative works, please contact ely.repiso@upc.edu to try to agree on 
+ *   collaborations (for journals with other researchers, formal collaborations between UPC and companies, and so on).
+ *
+ *  Redistribution and use in source and binary forms, without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission. 
+ *   * Additionally, remember that the derivatives without collaboration 
+ *     Of the original authors are prohibited
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
 #ifndef prediction_bhmip_H
 #define prediction_bhmip_H
 
